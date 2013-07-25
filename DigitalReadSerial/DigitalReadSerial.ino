61a8b650e4553d6a9ac0ee678f654f9f788198fe
Casey Grzecka <c@sey.gr>
2013-07-25 02:37:20 -0700

    Added first night of tutorial learning arduino development

---
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -0,0 +1,36 @@
+/*
+  DigitalReadSerial
+ Reads a digital input on pin 2, prints the result to the serial monitor 
+ 
+ This example code is in the public domain.
+ */
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+int pushButton = 2;
+// give it a name:
+int led = 13;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int buttonState = digitalRead(pushButton);
+
+  if (buttonState == 1) {
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
+  // print out the state of the button:
+  Serial.println(buttonState);
+
+  delay(1);        // delay in between reads for stability
+}
